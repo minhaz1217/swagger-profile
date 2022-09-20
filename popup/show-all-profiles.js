6b2335ab23fd0950642fe566e35f2833acc96b7b
Minhaz <minhaz1217@gmail.com>
2022-09-21 00:35:16 +0600

    apply token working from popup

--- popup/show-all-profiles.js
+++ popup/show-all-profiles.js
@@ -1,27 +1,42 @@
-async function loadAllProfiles(){
+async function loadAllProfiles() {
     let data = await getStorageData("profiles");
     let profilesDiv = $("#profiles");
-    if ( !(data == null || data?.profiles == null || !Array.isArray(data?.profiles)) ) {
+    console.log($("#profiles"));
+    if (!(data == null || data?.profiles == null || !Array.isArray(data?.profiles))) {
         // we have some profiles
         let profilesHtml = "";
-        
-        data.profiles.sort((a, b)=> {return a.displayOrder >= b.displayOrder});
-        data.profiles.forEach( (profile)=>{
+
+        data.profiles.sort((a, b) => { return a.displayOrder >= b.displayOrder });
+        data.profiles.forEach((profile) => {
             profilesHtml += makeSingleProfile(profile);
-        } );
+        });
         profilesDiv.html(profilesHtml);
-    }else{
-        profilesDiv.html("No profiles are present, please insert a profiles");
+        afterProfilesLoads();
+    } else {
+        profilesDiv.html("No profiles are present, please add new profile.");
     }
 }
 
-function makeSingleProfile(profile){
+function makeSingleProfile(profile) {
     return `
     <div class="mb-2 row">
         <div class="h2 col-6">${profile.name}</div>
-        <button class="col-6 btn btn-primary">Apply</button>
+        <button class="col-6 btn btn-primary applyButton" data-token="${profile.token}">Apply</button>
     </div>
         `;
+}
+
+function afterProfilesLoads() {
+    $(".applyButton").on("click", (e) => {
+        applyToken(e.currentTarget.getAttribute("data-token"));
+    });
+}
+
+function applyToken(token) {
+    if(token == null || token == ""){
+        return;
+    }
+    changeBearerToken(token);
 }
 
 loadAllProfiles();
