b6660237971b161d46bf2aee300b1503ab896eaf
Minhaz <minhaz1217@gmail.com>
2022-09-27 00:43:35 +0600

    added form validation for the profile add fields, on pressing save.

--- popup/add-new-profile.js
+++ popup/add-new-profile.js
@@ -1,6 +1,9 @@
 document.querySelector("#saveToken").addEventListener('click', saveToken);
 
 async function saveToken() {
+    if (!validateInputFields()) {
+        return;
+    }
     var profile = {
         name: $("#name").val(),
         token: $("#token").val(),
@@ -9,6 +12,72 @@ async function saveToken() {
     await addNewProfile(profile);
 }
 
+function validateNameField(){
+    let nameField = $("#name");
+    let nameError = $("#error-name");
+    let error = false;
+
+    if (nameField.val() == null || nameField.val() == "") {
+        let errorMessage = "Please enter a name for the profile";
+        nameError.html(errorMessage);
+        nameError.removeClass("visually-hidden");
+        nameField.addClass("is-invalid");
+        error = true;
+    } else {
+        nameError.addClass("visually-hidden");
+        nameField.removeClass("is-invalid");
+    }
+    return !error; // error == true means validation failed so we return false from the validation;
+}
+
+function validateTokenField(){
+    let tokenField = $("#token");
+    let tokenError = $("#error-token");
+    let error = false;
+
+    if (tokenField.val() == null || tokenField.val() == "") {
+        let errorMessage = "Please enter a token for the profile";
+        tokenError.html(errorMessage);
+        tokenError.removeClass("visually-hidden");
+        tokenField.addClass("is-invalid");
+        error = true;
+    } else {
+        tokenError.addClass("visually-hidden");
+        tokenField.removeClass("is-invalid");
+    }
+    return !error; // error == true means validation failed so we return false from the validation;
+}
+
+function validateDisplayOrder(){
+    let displayOrderField = $("#displayOrder");
+    let displayOrderError = $("#error-displayOrder");
+    let error = false;
+
+    if (displayOrderField.val() == null || displayOrderField.val() == "") {
+        let errorMessage = "Please enter a display order for the profile";
+        displayOrderError.html(errorMessage);
+        displayOrderError.removeClass("visually-hidden");
+        displayOrderField.addClass("is-invalid");
+        error = true;
+    } else {
+        displayOrderError.addClass("visually-hidden");
+        displayOrderField.removeClass("is-invalid");
+    }
+    return !error; // error == true means validation failed so we return false from the validation;
+}
+
+function validateInputFields() {
+    let nameFieldValidated = validateNameField();
+    let tokenFieldValidated = validateTokenField();
+    let displayOrderFieldValidated = validateDisplayOrder();
+    if( !nameFieldValidated || !tokenFieldValidated || !displayOrderFieldValidated ){
+        return false;
+    }
+    return true;
+}
+validateInputFields();
+
 // TODO: add validation for empty
 // TODO: in successful save, show success message and the go back to previous screen.
 // TODO: remove all online css and js, use only offline css and js (icons)
+// TODO: bug - display order is being saved as string, save it as number or sort it as number.
