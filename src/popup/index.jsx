e5b99d70e47065c890aa9f331d4f0f98d9f1452d
Minhaz <minhaz1217@gmail.com>
2022-10-09 01:09:43 +0600

    manifest to v3 and updated todo

--- src/popup/index.jsx
+++ src/popup/index.jsx
@@ -10,12 +10,12 @@ ReactDOM.createRoot(document.getElementById("react-app")).render(
     </React.StrictMode>
 );
 
-// TODO: make the extension fully functional
+// TODO: DONE - make the extension fully functional
 // TODO: in successful save, show success message and the go back to previous screen.
-// TODO: on submit and successful save, redirect to show all page.
+// TODO: DONE - on submit and successful save, redirect to show all page.
 // TODO: add css minifier for bootstrap to only use css that we used in our files, just like tailwind
 // TODO: use jslint
 // TODO: add tailwind support
 // TODO: add typescript support
-// TODO: rename popup to show all page
+// TODO: DONE - rename popup to show all page
 // TODO: add in memory router.
