12971635c63aad0346e24c28900cb13e8e9906da
Minhaz <minhaz1217@gmail.com>
2022-10-10 18:18:29 +0600

    added capabilities text

--- src/popup/index.jsx
+++ src/popup/index.jsx
@@ -20,3 +20,15 @@ ReactDOM.createRoot(document.getElementById("react-app")).render(
 // TODO: DONE - rename popup to show all page
 // TODO: add in memory router.
 // TODO: don't use promised based, because of polyfill.
+
+// Capabilities (for testing purposes)
+// 1. When clicking extension button for the first time, it will show that there is no profiles
+// 2. Clicking add will redirect to the add new profile page.
+// 3. Filling up form will add the profile
+// 4. After saving the profile it will redirect to show all profile page.
+// 5. Show all profiles will show all the save profiles
+// 6. Clicking apply will apply that profile
+// 7. Clicking again in another profile will apply that profile removing the old one.
+// 8. Clicking edit will let the user edit that profile
+// 9. Clicking delete will delete that profile
+// 10. While editing if the user changes the display order it will re-order that profile.
