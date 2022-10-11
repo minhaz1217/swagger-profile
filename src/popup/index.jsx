e7e6251029db6f9540f15bd1d335fafffd5908ef
Minhaz <minhaz1217@gmail.com>
2022-10-11 15:50:32 +0600

    new in UI confirmation added for delete

--- src/popup/index.jsx
+++ src/popup/index.jsx
@@ -20,7 +20,7 @@ ReactDOM.createRoot(document.getElementById("react-app")).render(
 // TODO: DONE - rename popup to show all page
 // TODO: add in memory router.
 // TODO: don't use promised based, because of polyfill.
-// TODO: BUG - deleting profile now working in chrome if the developer tool isn't open.
+// TODO: DONE - BUG - deleting profile now working in chrome if the developer tool isn't open.
 
 
 // Capabilities (for testing purposes)
--- src/popup/profile.jsx
+++ src/popup/profile.jsx
@@ -5,6 +5,8 @@ import { deleteProfile } from "../services/SwaggerProfileService.js";
 import { changeBearerToken, deleteProfileWebConfirmation } from "../services/TokenService.js"
 const Profile = ({ profile, onProfileChangeCallback }) => {
     const navigate = useNavigate();
+    const [showConfirm, setShowConfirm] = useState(false);
+
 
     const onClickApplyButton = () => {
         if (profile?.token == null || profile?.token == "") {
@@ -20,29 +22,55 @@ const Profile = ({ profile, onProfileChangeCallback }) => {
         });
     }
     const onClickDeleteButton = async () => {
+        setShowConfirm(true);
+    }
+
+
+    const onConfirmAccept = async () => {
         if (profile?.token == null || profile?.token == "") {
             return;
         }
         let profilesDeleted = await deleteProfile(profile?.id);
+        setShowConfirm(true);
         if (profilesDeleted === true) {
             onProfileChangeCallback();
         }
     }
-
+    const onConfirmReject = () => {
+        setShowConfirm(false);
+    }
     return (
-        <div className="mb-2 row">
-            <div className="h2 col-6">{profile.name}</div>
-            <div className="col-6">
-                <button className="btn btn-success me-1 applyButton" data-token={profile.token} title="Apply profile" onClick={onClickApplyButton} >
-                    <i className="bi bi-check2-circle"></i>
-                </button>
-                <button className="btn btn-warning me-1 editButton" data-id={profile.id} title="Edit" onClick={onClickEditButton}>
-                    <i className="bi bi-pencil-square"></i>
-                </button>
-                <button className="btn btn-danger deleteButton" data-id={profile.id} title="Delete" onClick={onClickDeleteButton}>
-                    <i className="bi bi-trash"></i>
-                </button>
-            </div>
+        <div>
+
+            {!showConfirm &&
+                <div className="mb-2 row">
+                    <div className="h2 col-6">{profile.name}</div>
+                    <div className="col-6">
+                        <button className="btn btn-success me-1 applyButton" data-token={profile.token} title="Apply profile" onClick={onClickApplyButton} >
+                            <i className="bi bi-check2-circle"></i>
+                        </button>
+                        <button className="btn btn-warning me-1 editButton" data-id={profile.id} title="Edit" onClick={onClickEditButton}>
+                            <i className="bi bi-pencil-square"></i>
+                        </button>
+                        <button className="btn btn-danger deleteButton" data-id={profile.id} title="Delete" onClick={onClickDeleteButton}>
+                            <i className="bi bi-trash"></i>
+                        </button>
+                    </div>
+                </div>
+            }
+            {showConfirm &&
+                <div className="mb-2 row">
+                    <div className="h2 col-6">Confirm?</div>
+                    <div className="col-6">
+                        <button className="btn btn-success me-1" data-token={profile.token} title="Yes" onClick={onConfirmAccept} >
+                            <i className="bi bi-check-lg"></i>
+                        </button>
+                        <button className="btn btn-danger" title="No" onClick={onConfirmReject}>
+                            <i className="bi bi-x-lg"></i>
+                        </button>
+                    </div>
+                </div>
+            }
         </div>
     );
 }
